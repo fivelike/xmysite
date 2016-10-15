861fa283a424afe75665cc52180fca95c26d093c
shrik3 <cshrik3@gmail.com>
2016-10-15 11:41:29 +0800

    PHP是世界上最好的语言

---
+++ php/register.php
@@ -0,0 +1,10 @@
+<?php
+
+
+    $username = $_POST['user'];
+    $password = $_POST['passwd'];
+    echo '<h1 align=center> Register Success!';
+    echo '<h2 align=center>Welcome '.$username.'!<h2>';
+
+
+?>
