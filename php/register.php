0932b5df474c9e2536af8d19cdc0a9780dc1a9af
shrik3 <cshrik3@gmail.com>
2016-10-17 20:11:06 +0800

    test

--- php/register.php
+++ php/register.php
@@ -1,10 +1,15 @@
 <?php
 
+    $db = new mysqli('localhost','root','','xmy');
+    if(mysqli_connect_errno()){
+        echo 'error'
+        exit;
+    }
 
     $username = $_POST['user'];
     $password = $_POST['passwd'];
     echo '<h1 align=center> Register Success!';
+    echo '<br>';
     echo '<h2 align=center>Welcome '.$username.'!<h2>';
-
 
 ?>
---
+++ php/sqltest.php
@@ -0,0 +1,8 @@
+<?php
+
+    $db = new mysqli('localhost','test','test123','xmy');
+    echo $db;
+
+
+
+?>
